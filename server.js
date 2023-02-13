d8ea8ffdf1ff5b236bcfd9e141f3cc2aaa6385bc
devarogundade <devarogundade@gmail.com>
2023-02-13 16:56:10 +0100

    added request routes to server

--- server.js
+++ server.js
@@ -1,45 +1,46 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
+const app = express()
 
 var corsOptions = {
     // origin: "http://localhost:8081"
-};
+}
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 // parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json())
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-const db = require("./app/models");
+const db = require("./app/models")
 db.mongoose
     .connect(db.url, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => {
-        console.log("Connected to the database!");
+        console.log("Connected to the database!")
     })
     .catch(err => {
-        console.log("Cannot connect to the database!", err);
-        process.exit();
-    });
+        console.log("Cannot connect to the database!", err)
+        process.exit()
+    })
 
-// simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Dash Protocol API V1" });
-});
+    res.json({ message: "Darsh Protocol API V1" })
+})
 
-require("./app/routes/offers/lending.routes")(app);
-require("./app/routes/offers/borrowing.routes")(app);
-require("./app/routes/loans/loans.routes")(app);
+require("./app/routes/loans/loans.routes")(app)
+require("./app/routes/offers/lending.routes")(app)
+require("./app/routes/offers/borrowing.routes")(app)
+require("./app/routes/requests/lending.routes.js")(app)
+require("./app/routes/requests/borrowing.routes.js")(app)
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+    console.log(`Server is running on port ${PORT}.`)
+})
