57ff932bd0e776d5cdc5c9a4956f4636da5b9d4a
devarogundade <devarogundade@gmail.com>
2023-02-22 08:04:24 +0100

    remove unused log statements

--- app/controllers/transfers.controller.js
+++ app/controllers/transfers.controller.js
@@ -11,8 +11,6 @@ exports.create = (req, res) => {
     const postData = moraliswebhook.resolve(req)
     if (postData == []) return res.send("No post data")
 
-    console.log('postData', postData);
-
     postData.forEach(_postData => {
         // Save or update Offer in the database
         Transfer.findOneAndUpdate(
